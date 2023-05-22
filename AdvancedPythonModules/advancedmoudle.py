49b14e2dfbe2eb469831b790c12a4b710f770a09
kadarbarnabas <113166617+kadarbarnabas@users.noreply.github.com>
2023-05-22 11:44:55 +0200

    advancedmoudle.py

--- AdvancedPythonModules/advancedmoudle.py
+++ AdvancedPythonModules/advancedmoudle.py
@@ -1,7 +1,7 @@
 import zipfile
 import os
 import re
-"""
+
 zip_obj = zipfile.ZipFile('c:/Users/Barnus/Desktop/python/pythontst/UdemyHomework/AdvancedPythonModules/unzip_me_for_instructions.zip', 'r')
 zip_obj.extractall()
 
@@ -11,7 +11,7 @@ print(conent)
 f.close()
 
 treelist = os.listdir('c:/Users/Barnus/Desktop/python/pythontst/UdemyHomework/AdvancedPythonModules/extracted_content')
-"""
+
 file_path = 'c:/Users/Barnus/Desktop/python/pythontst/UdemyHomework/AdvancedPythonModules/extracted_content'
 for folder, sub_folder, files in os.walk(file_path): 
     for f in files:
